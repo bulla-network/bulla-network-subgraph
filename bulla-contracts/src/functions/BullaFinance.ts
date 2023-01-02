c884f4d1d705b8b639b0a898b8b0bb514f0c3a13
Colin Nielsen <colinrnielsen@gmail.com>
2023-01-02 14:25:01 -0700

    fix typo

--- bulla-contracts/src/functions/BullaFinance.ts
+++ bulla-contracts/src/functions/BullaFinance.ts
@@ -2,8 +2,8 @@ import { BigInt, ethereum } from "@graphprotocol/graph-ts";
 import { FinancingOffered, FinancingAccepted } from "../../generated/BullaFinance/BullaFinance";
 import { FinancingOfferedEvent, FinancingAcceptedEvent } from "../../generated/schema";
 
-export const getFinancingOfferedEventId = (underlyingTokenId: BigInt, event: ethereum.Event): string =>
-  "FinancingOffered-" + underlyingTokenId.toString() + "-" + event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+export const getFinancingOfferedEventId = (underlyingClaimId: BigInt, event: ethereum.Event): string =>
+  "FinancingOffered-" + underlyingClaimId.toString() + "-" + event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
 
 export const getFinancingAcceptedEventId = (underlyingTokenId: BigInt, financedTokenId: BigInt, event: ethereum.Event): string =>
   "FinancingAccepted-" + underlyingTokenId.toString() + "-" + financedTokenId.toString() + "-" + event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
