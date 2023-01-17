7f4f0e0b617a7382250ffdd6abd2bf68d35bbc87
Colin Nielsen <colinrnielsen@gmail.com>
2023-01-17 15:53:28 -0700

    feat: :sparkles: add FrendLend event indexing

---
+++ bulla-contracts/src/functions/FrendLend.ts
@@ -0,0 +1,29 @@
+import { BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { LoanOfferAccepted, LoanOffered, LoanOfferRejected } from "../../generated/FrendLend/FrendLend";
+import { LoanOfferAcceptedEvent, LoanOfferedEvent, LoanOfferRejectedEvent } from "../../generated/schema";
+
+export const getLoanOfferAcceptedEventId = (loanId: BigInt, claimId: BigInt, event: ethereum.Event): string =>
+  "LoanOfferAccepted-" + loanId.toString() + "-" + claimId.toString() + "-" + event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+
+export const getLoanOfferRejectedEventId = (loanId: BigInt, event: ethereum.Event): string =>
+  "LoanOfferRejected-" + loanId.toString() + "-" + event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+
+export const loadLoanOfferedEvent = (loanId: string, createOnNull: boolean): LoanOfferedEvent => {
+  let loanEvent = LoanOfferedEvent.load(loanId);
+  if (!loanEvent) {
+    if (createOnNull) loanEvent = new LoanOfferedEvent(loanId);
+    else throw new Error("Loan Offer Event not found");
+  }
+
+  return loanEvent;
+};
+
+export const createLoanOfferedEvent = (event: LoanOffered): LoanOfferedEvent => loadLoanOfferedEvent(event.params.loanId.toString(), true);
+
+export const getLoanOfferedEvent = (loanId: string): LoanOfferedEvent => loadLoanOfferedEvent(loanId, false);
+
+export const createLoanOfferAcceptedEvent = (event: LoanOfferAccepted): LoanOfferAcceptedEvent =>
+  new LoanOfferAcceptedEvent(getLoanOfferAcceptedEventId(event.params.loanId, event.params.claimId, event));
+
+export const createLoanOfferRejectedEvent = (event: LoanOfferRejected): LoanOfferRejectedEvent =>
+  new LoanOfferRejectedEvent(getLoanOfferRejectedEventId(event.params.loanId, event));
---
+++ bulla-contracts/src/mappings/FrendLend.ts
@@ -0,0 +1,97 @@
+import { BullaTagUpdated } from "../../generated/BullaBanker/BullaBanker";
+import { LoanOfferAccepted, LoanOffered, LoanOfferRejected } from "../../generated/FrendLend/FrendLend";
+import { getIPFSHash, getOrCreateToken, getOrCreateUser } from "../functions/common";
+import { createLoanOfferAcceptedEvent, createLoanOfferedEvent, createLoanOfferRejectedEvent, getLoanOfferedEvent } from "../functions/FrendLend";
+import * as BullaBanker from "./BullaBanker";
+
+// this contract also emits BullaTagUpdatedEvents
+export function handleBullaTagUpdated(event: BullaTagUpdated): void {
+  BullaBanker.handleBullaTagUpdated(event);
+}
+
+export function handleLoanOffered(event: LoanOffered): void {
+  const ev = event.params;
+  const offer = event.params.loanOffer;
+
+  const loanOfferedEvent = createLoanOfferedEvent(event);
+
+  const user_creditor = getOrCreateUser(offer.creditor);
+  const user_debtor = getOrCreateUser(offer.debtor);
+
+  loanOfferedEvent.offeredBy = ev.offeredBy;
+  loanOfferedEvent.interestBPS = offer.interestBPS;
+  loanOfferedEvent.termLength = offer.termLength;
+  loanOfferedEvent.loanAmount = offer.loanAmount;
+  loanOfferedEvent.creditor = offer.creditor;
+  loanOfferedEvent.debtor = offer.debtor;
+  loanOfferedEvent.description = offer.description;
+  loanOfferedEvent.claimToken = getOrCreateToken(offer.claimToken).id;
+  loanOfferedEvent.ipfsHash = getIPFSHash(offer.attachment);
+
+  loanOfferedEvent.eventName = "LoanOffered";
+  loanOfferedEvent.blockNumber = event.block.number;
+  loanOfferedEvent.transactionHash = event.transaction.hash;
+  loanOfferedEvent.logIndex = event.logIndex;
+  loanOfferedEvent.timestamp = event.block.timestamp;
+
+  user_creditor.frendLendEvents = user_creditor.frendLendEvents ? user_creditor.frendLendEvents.concat([loanOfferedEvent.id]) : [loanOfferedEvent.id];
+  user_debtor.frendLendEvents = user_debtor.frendLendEvents ? user_debtor.frendLendEvents.concat([loanOfferedEvent.id]) : [loanOfferedEvent.id];
+
+  loanOfferedEvent.save();
+  user_creditor.save();
+  user_debtor.save();
+}
+
+export function handleLoanOfferAccepted(event: LoanOfferAccepted): void {
+  const ev = event.params;
+  const loanId = event.params.loanId;
+
+  const loanOfferAcceptedEvent = createLoanOfferAcceptedEvent(event);
+  const loanOfferedEvent = getLoanOfferedEvent(loanId.toString());
+
+  const user_creditor = getOrCreateUser(loanOfferedEvent.creditor);
+  const user_debtor = getOrCreateUser(loanOfferedEvent.debtor);
+
+  loanOfferAcceptedEvent.loanId = loanId.toString();
+  loanOfferAcceptedEvent.claimId = ev.claimId.toString();
+
+  loanOfferAcceptedEvent.eventName = "LoanOfferAccepted";
+  loanOfferAcceptedEvent.blockNumber = event.block.number;
+  loanOfferAcceptedEvent.transactionHash = event.transaction.hash;
+  loanOfferAcceptedEvent.logIndex = event.logIndex;
+  loanOfferAcceptedEvent.timestamp = event.block.timestamp;
+
+  user_creditor.frendLendEvents = user_creditor.frendLendEvents ? user_creditor.frendLendEvents.concat([loanOfferAcceptedEvent.id]) : [loanOfferAcceptedEvent.id];
+  user_debtor.frendLendEvents = user_debtor.frendLendEvents ? user_debtor.frendLendEvents.concat([loanOfferAcceptedEvent.id]) : [loanOfferAcceptedEvent.id];
+
+  loanOfferAcceptedEvent.save();
+  user_creditor.save();
+  user_debtor.save();
+}
+
+export function handleLoanOfferRejected(event: LoanOfferRejected): void {
+  const ev = event.params;
+  const loanId = event.params.loanId;
+
+  const loanOfferRejectedEvent = createLoanOfferRejectedEvent(event);
+  const loanOfferedEvent = getLoanOfferedEvent(loanId.toString());
+
+  const user_creditor = getOrCreateUser(loanOfferedEvent.creditor);
+  const user_debtor = getOrCreateUser(loanOfferedEvent.debtor);
+
+  loanOfferRejectedEvent.loanId = loanId.toString();
+  loanOfferRejectedEvent.rejectedBy = ev.rejectedBy;
+
+  loanOfferRejectedEvent.eventName = "LoanOfferLoanOfferRejected";
+  loanOfferRejectedEvent.blockNumber = event.block.number;
+  loanOfferRejectedEvent.transactionHash = event.transaction.hash;
+  loanOfferRejectedEvent.logIndex = event.logIndex;
+  loanOfferRejectedEvent.timestamp = event.block.timestamp;
+
+  user_creditor.frendLendEvents = user_creditor.frendLendEvents ? user_creditor.frendLendEvents.concat([loanOfferRejectedEvent.id]) : [loanOfferRejectedEvent.id];
+  user_debtor.frendLendEvents = user_debtor.frendLendEvents ? user_debtor.frendLendEvents.concat([loanOfferRejectedEvent.id]) : [loanOfferRejectedEvent.id];
+
+  loanOfferRejectedEvent.save();
+  user_creditor.save();
+  user_debtor.save();
+}
