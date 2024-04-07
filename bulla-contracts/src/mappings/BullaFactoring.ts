413803cf1de65db147506d872d838f911827e5d6
solidoracle <105349716+solidoracle@users.noreply.github.com>
2024-04-07 21:58:20 +0200

    setup factoring events (#29)

---
+++ bulla-contracts/src/mappings/BullaFactoring.ts
@@ -0,0 +1,23 @@
+import { InvoiceFunded } from "../../generated/BullaFactoring/BullaFactoring";
+import { getClaim } from "../functions/BullaClaimERC721";
+import { createInvoiceFundedEvent } from "../functions/BullaFactoring";
+
+export function handleInvoiceFunded(event: InvoiceFunded): void {
+  const ev = event.params;
+  const originatingClaimId = ev.invoiceId;
+
+  const underlyingClaim = getClaim(originatingClaimId.toString());
+  const InvoiceFundedEvent = createInvoiceFundedEvent(originatingClaimId, event);
+
+  InvoiceFundedEvent.invoiceId = underlyingClaim.id;
+  InvoiceFundedEvent.fundedAmount = ev.fundedAmount;
+  InvoiceFundedEvent.originalCreditor = ev.originalCreditor;
+
+  InvoiceFundedEvent.eventName = "InvoiceFunded";
+  InvoiceFundedEvent.blockNumber = event.block.number;
+  InvoiceFundedEvent.transactionHash = event.transaction.hash;
+  InvoiceFundedEvent.logIndex = event.logIndex;
+  InvoiceFundedEvent.timestamp = event.block.timestamp;
+
+  InvoiceFundedEvent.save();
+}
